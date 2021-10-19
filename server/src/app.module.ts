dfa571360dcff82ea8058066fe53c2c449d93a8f
Eduard Sobotnyk <djedj508@gmail.com>
2021-10-19 20:46:59 +0300

    added typeorm + config module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({ isGlobal: true }),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
