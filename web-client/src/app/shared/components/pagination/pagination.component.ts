96b4bca95e84df750a42b6bfb363697123ef544f
Eduard Sobotnyk <djedj508@gmail.com>
2021-10-21 16:11:29 +0300

    improved pagination algo

--- web-client/src/app/shared/components/pagination/pagination.component.ts
+++ web-client/src/app/shared/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 import { IPaginationResult } from '../../models/filter.model';
 
@@ -7,23 +7,65 @@ import { IPaginationResult } from '../../models/filter.model';
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
   @Input() options!: IPaginationResult;
 
   @Output() paginate: EventEmitter<Partial<IPaginationResult>> = new EventEmitter<Partial<IPaginationResult>>();
 
-  get pages() {
-    return new Array(Math.ceil(this.options.total / this.options.perPage))
-      .fill(0)
-      .map((_, i) => i + 1);
-  }
+  pages: Array<string | number> = [];
 
   constructor() {}
 
-  selectPage(page: number) {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.options && changes.options.currentValue) {
+      this.pages = this.paginationGenerator(
+        this.options.page,
+        Math.ceil(this.options.total / this.options.perPage),
+      );
+    }
+  }
+
+  selectPage(page: number | string) {
     this.paginate.emit({
-      page,
+      page: Number(page),
       perPage: this.options.perPage,
     });
+  }
+
+  paginationGenerator(current: number, last: number, width = 2) {
+    const left = current - width;
+    const right = current + width + 1;
+    const range = [];
+    const rangeWithDots: Array<number | string> = [];
+    let l: number;
+
+    for (let i = 1; i <= last; i += 1) {
+      if (i === 1 || i === last || (i >= left && i <= right)) {
+        range.push(i);
+      } else if (i < left) {
+        i = left - 1;
+      } else if (i > right) {
+        range.push(last);
+        break;
+      }
+    }
+
+    range.forEach(i => {
+      if (l) {
+        if (i - l === 2) {
+          rangeWithDots.push(l + 1);
+        } else if (i - l !== 1) {
+          rangeWithDots.push('...');
+        }
+      }
+      rangeWithDots.push(i);
+      l = i;
+    });
+
+    return rangeWithDots;
+  };
+
+  isClickable(page: number | string) {
+    return page !== '...';
   }
 }
